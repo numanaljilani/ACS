f64adb53e3a94f554e97342781cee63908b88834
numan <numanaljilani001.com>
2025-06-10 22:41:16 +0530

    check

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,35 +1,30 @@
-import { NextIntlClientProvider } from "next-intl";
-import { notFound } from "next/navigation";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import "../globals.css";
-import { ReactNode } from "react";
-
+import { NextIntlClientProvider } from 'next-intl';
+import { notFound } from 'next/navigation';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import '../globals.css';
+import { ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
-  params: {
-    locale: string;
-  };
+  params: Promise <any>;
 };
+
 export default async function LocaleLayout({
   children,
-  params: { locale },
-}:Props) {
+  params,
+}: Props) {
+ const  { locale } = await params
   let messages;
   try {
     messages = (await import(`../../messages/${locale}.json`)).default;
   } catch (error) {
-    console.log(error)
-    notFound();
+    console.error(`Could not load messages for locale "${locale}"`);
+    return notFound(); // ✅ FIX: return it
   }
 
   return (
-    <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
-      {/* <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Amiri:wght@400;700&display=swap"
-        rel="stylesheet"
-      /> */}
+    <html lang={locale} dir={locale === 'ar' ? 'rtl' : 'ltr'}>
       <body>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Header />
